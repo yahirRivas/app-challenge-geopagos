febf22c08d5de91e057b390e96ac104386da522e
Yahir <yahir.14.rivas@gmail.com>
2022-10-16 15:14:44 -0500

    init project

---
+++ src/main/java/com/geopagos/torneo/AppChallengeGeopagosApplication.java
@@ -0,0 +1,13 @@
+package com.geopagos.torneo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppChallengeGeopagosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppChallengeGeopagosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geopagos/torneo/AppChallengeGeopagosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geopagos.torneo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppChallengeGeopagosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
